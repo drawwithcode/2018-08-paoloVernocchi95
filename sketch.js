9a8917bbe80ce3bbc08027120e9a055eb6ec692e
paoloVernocchi95 <43385409+paoloVernocchi95@users.noreply.github.com>
2018-11-20 19:40:05 +0100

    martedì 19.39

--- sketch.js
+++ sketch.js
@@ -1,60 +1,89 @@
 function preload(){
   // put preload code here
-  img = loadImage('palloncino.png');
 }
 var hit = false;
 var balls = [];
 var poly = [];
+var poly2 = [];
 function setup() {
   createCanvas(windowWidth,windowHeight);
   textAlign(CENTER);
   angleMode(DEGREES);
   collideDebug(true)
-	poly[0] = createVector(123,231);     // set X/Y position
-	poly[1] = createVector(10,111);
-	poly[2] = createVector(20,23);
-	poly[3] = createVector(390,33);
-  /*var goodBall = new Ball(10, 10, 100);
-  balls.push(goodBall);*/
+  poly[0] = createVector(0,0);     // set X/Y position
+  poly[1] = createVector(random(10,windowWidth/2),random(0, windowHeight/16) );
+  poly[2] = createVector(random(10,windowWidth/2),random( windowHeight/16, windowHeight/16*2) );
+  poly[3] = createVector(random(10,windowWidth/2),random( windowHeight/16*2, windowHeight/16*3) );
+  poly[4] = createVector(random(10,windowWidth/2),random( windowHeight/16*3, windowHeight/16*4) );
+  poly[5] = createVector(random(10,windowWidth/2),random( windowHeight/16*4, windowHeight/16*5) );
+  poly[6] = createVector(random(10,windowWidth/2),random( windowHeight/16*5, windowHeight/16*6) );
+  poly[7] = createVector(random(10,windowWidth/2),random( windowHeight/16*6, windowHeight/16*7) );
+  poly[8] = createVector(random(10,windowWidth/2),random( windowHeight/16*7, windowHeight/16*8) );
+  poly[9] = createVector(random(10,windowWidth/2),random( windowHeight/16*8, windowHeight/16*9) );
+  poly[10] = createVector(random(10,windowWidth/2),random( windowHeight/16*9, windowHeight/16*10) );
+  poly[11] = createVector(random(10,windowWidth/2),random( windowHeight/16*10, windowHeight/16*11) );
+  poly[12] = createVector(random(10,windowWidth/2),random( windowHeight/16*11, windowHeight/16*12) );
+  poly[13] = createVector(random(10,windowWidth/2),random( windowHeight/16*12, windowHeight/16*13) );
+  poly[14] = createVector(random(10,windowWidth/2),random( windowHeight/16*13, windowHeight/16*14) );
+  poly[15] = createVector(random(10,windowWidth/2),random( windowHeight/16*14, windowHeight/16*15) );
+  poly[16] = createVector(random(10,windowWidth/2),random( windowHeight/16*15, windowHeight/16*16) );
+    poly[16] = createVector(0,random( windowHeight/16*16, windowHeight/16*17));
+
+    poly2[0] = createVector(windowHeight,0);     // set X/Y position
+    poly2[1] = createVector(random( windowWidth/2,windowWidth ),random(0, windowHeight/16) );
+    poly2[2] = createVector(random( windowWidth/2,windowWidth ),random( windowHeight/16, windowHeight/16*2) );
+    poly2[3] = createVector(random( windowWidth/2,windowWidth ),random( windowHeight/16*2, windowHeight/16*3) );
+    poly2[4] = createVector(random( windowWidth/2,windowWidth ),random( windowHeight/16*3, windowHeight/16*4) );
+    poly2[5] = createVector(random( windowWidth/2,windowWidth ),random( windowHeight/16*4, windowHeight/16*5) );
+    poly2[6] = createVector(random( windowWidth/2,windowWidth ),random( windowHeight/16*5, windowHeight/16*6) );
+    poly2[7] = createVector(random( windowWidth/2,windowWidth ),random( windowHeight/16*6, windowHeight/16*7) );
+    poly2[8] = createVector(random( windowWidth/2,windowWidth ),random( windowHeight/16*7, windowHeight/16*8) );
+    poly2[9] = createVector(random( windowWidth/2,windowWidth ),random( windowHeight/16*8, windowHeight/16*9) );
+    poly2[10] = createVector(random( windowWidth/2,windowWidth ),random( windowHeight/16*9, windowHeight/16* 10) );
+    poly2[11] = createVector(random( windowWidth/2,windowWidth ),random( windowHeight/16*10, windowHeight/16*11) );
+    poly2[12] = createVector(random( windowWidth/2,windowWidth ),random( windowHeight/16*11, windowHeight/16*12) );
+    poly2[13] = createVector(random( windowWidth/2,windowWidth ),random( windowHeight/16*12, windowHeight/16*13) );
+    poly2[14] = createVector(random( windowWidth/2,windowWidth ),random( windowHeight/16*13, windowHeight/16*14) );
+    poly2[15] = createVector(random( windowWidth/2,windowWidth ),random( windowHeight/16*14, windowHeight/16*15) );
+    poly2[16] = createVector(random( windowWidth/2,windowWidth ),random( windowHeight/16*15, windowHeight/16*16) );
+      poly2[16] = createVector(windowHeight,random( windowHeight/16*16, windowHeight/16*17));
+
+
+
+
+
 }
 
 function draw() {
-background(255);
 /*for (var j = 0; j < balls.length; j++) {
   balls[j].move();
   balls[j].display();
 }*/
+background(255);
 rz= rotationZ;
 rx= rotationX;
 ry= rotationY;
 beginShape();
-	for(i=0; i < poly.length; i++){
-		vertex(poly[i].x,poly[i].y);
-	}
-	endShape(CLOSE);
+for(i=0; i < 17; i++){
+  vertex(poly[i].x,poly[i].y);
+}
+endShape(CLOSE);
+beginShape();
+for(j=0; j < 17; j++){
+  vertex(poly2[j].x,poly2[j].y);
+}
+endShape(CLOSE);
+
 
 push();
 //translate(windowWidth/2,windowHeight/2);
 rotate(90);
 //image(img,10*rx+windowWidth/2,-25*ry , img.width/25, img.height/25);
 pop();
-	rect(200,200,windowWidth/2,windowHeight/2);
-  ellipse (10*ry+windowWidth/2,25*rx+100,30);
-	hit = collideRectCircle(200,200,100,150,10*ry+windowWidth/2,25*rx+100,30);
-  ellipse(windowWidth/2,windowHeight -30,100,100);
-  	hit2 = collideCircleCircle(10*ry+windowWidth/2,25*rx+100,30,windowWidth/2,windowHeight -30,100,100)
-    hit3 = collideCirclePoly(10*ry+windowWidth/2,25*rx+100,30,poly);
-  if(hit == true){
-    		background(255,0,0,100);
-        push();
-        translate(windowWidth/2,windowHeight/2);
-        rotate(90);
-        fill(0);
-    text("GAME OVER",0,0);
-      text("REFRESH THE PAGE TO PLAY AGAIN ",0,30);
-	print("colliding? " + hit);
-  pop();
-}
+  ellipse (10*ry+windowWidth/2,25*rx+100,15);
+  ellipse(windowWidth/2,windowHeight -30,50,50);
+  	hit2 = collideCircleCircle(10*ry+windowWidth/2,25*rx+100,15,windowWidth/2,windowHeight -30,100,100)
+    hit3 = collideCirclePoly(10*ry+windowWidth/2,25*rx+100,15,poly)
 if(hit2 == true){
   push();
   translate(windowWidth/2,windowHeight/2);
